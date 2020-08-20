e13b01ac33a93f5001b9be8146d8e472b6c4706f
RomanTotskiy <69465800+RomanTotskiy@users.noreply.github.com>
2020-08-20 13:01:24 +0300

    Add files via upload

---
+++ day3/changedForTESTng/AlertDemo.java
@@ -0,0 +1,64 @@
+package org.example.day3changedForTESTng;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class AlertDemo {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void Openalert() {
+        System.setProperty("webdriver.chrome.driver","C:\WebDrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    @Test
+    public void alertDemo() {
+
+        // Alert Message handling
+        driver.get("http://demo.guru99.com/test/delete_customer.php");
+
+        driver.findElement(By.name("cusid")).sendKeys("53920");
+        driver.findElement(By.name("submit")).click();
+
+        wait.until(ExpectedConditions.alertIsPresent());
+        // Switching to Alert
+        Alert alert = driver.switchTo().alert();
+
+        // Capturing alert message.
+        String alertMessage= alert.getText();
+
+        // Displaying alert message
+        System.out.println(alertMessage);
+        // Accepting alert
+        alert.accept();
+
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        // Switching to Alert
+        Alert alert2 = driver.switchTo().alert();
+
+        // Capturing alert message.
+        String alertMessage2 = driver.switchTo().alert().getText();
+        System.out.println(alertMessage2);
+
+        alert2.accept();
+
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
